bd46eec3697bea2f59a96bb90fe984217da71f72
nnivxix <adhe.hanasa@gmail.com>
2023-09-24 15:46:45 +0700

    fix: user controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -38,6 +38,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
+        $user = $user->load('experiences');
         return UserResource::make($user);
     }
 
