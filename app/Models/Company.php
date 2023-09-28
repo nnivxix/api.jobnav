9aab0b91df292782194313017a09cef4e19674eb
nnivxix <adhe.hanasa@gmail.com>
2023-09-28 23:30:13 +0700

    fix: delete assets attachment when deleting companies

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use App\Models\Concerns\HasUrlAsset;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Company extends Model
 {
@@ -24,9 +25,31 @@ class Company extends Model
         return 'slug';
     }
 
+    protected static function boot()
+    {
+        parent::boot();
+        static::deleting(function ($company) {
+            $jobs = $company->jobs;
+
+            foreach ($jobs as $job) {
+                foreach ($job->applyJobs as $applyJob) {
+                    if (Storage::disk('public')->exists($applyJob->attachment)) {
+                        Storage::disk('public')->delete($applyJob->attachment);
+                    }
+                }
+            }
+        });
+    }
+
+
     public function jobs(): HasMany
     {
         return $this->hasMany(Job::class);
+    }
+
+    public function applyJobs()
+    {
+        return $this->hasManyThrough(ApplyJob::class, Job::class, 'company_id', 'job_id');
     }
 
     public function owner()
