b87015d7ffca88b7360031e55459df178877cce3
nnivxix <adhe.hanasa@gmail.com>
2023-10-01 21:22:08 +0700

    feat: user can view recent job applicant

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\JobResource;
+use App\Models\ApplyJob;
 use App\Models\Job;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
@@ -33,6 +34,18 @@ class JobController extends Controller
 
     public function show(Job $job)
     {
-        return JobResource::make($job);
+        $applyJob = null;
+
+        if (auth('sanctum')->check()) {
+            $applyJob = ApplyJob::query()
+            ->where('job_id', $job->id)
+            ->where('user_id', auth('sanctum')->user()->id)
+            ->first(); 
+        }
+        
+        return JobResource::make($job)
+        ->additional([
+            'job_status' => $applyJob
+        ]);
     }
 }
