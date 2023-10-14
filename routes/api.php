8ceef0562f1ac2f6e67a5cfde5578ed39d51a5f0
nnivxix <adhe.hanasa@gmail.com>
2023-10-14 21:21:30 +0700

    tes: show personal jobs

--- routes/api.php
+++ routes/api.php
@@ -39,10 +39,10 @@ Route::middleware('auth:sanctum',)->group(function () {
 
     Route::post('/jobs/{job}/apply', [ApplyJobController::class, 'store'])->name('apply-job.store');
 
-    Route::get('/personal-jobs', [PersonalJobController::class, 'index']);
+    Route::get('/personal-jobs', [PersonalJobController::class, 'index'])->name('personal-job.index');
     Route::post('/personal-jobs', [PersonalJobController::class, 'store']);
     Route::put('/personal-jobs/{uuid}', [PersonalJobController::class, 'update']);
-    Route::get('/personal-jobs/{job}', [PersonalJobController::class, 'show']);
+    Route::get('/personal-jobs/{job}', [PersonalJobController::class, 'show'])->name('personal-job.show');
     Route::delete('/personal-jobs/{job}', [PersonalJobController::class, 'destroy']);
 
     Route::get('/personal-jobs/{job}/user', [PersonalJobUserController::class, 'show']);
---
+++ tests/Feature/Http/Controllers/PersonalJobControllerTest.php.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Models\Job;
+use App\Models\User;
+use App\Models\Company;
+use App\Models\ApplyJob;
+
+beforeEach(function () {
+
+    $user = User::create([
+        'name'     => 'Hanasa',
+        'username' => 'hanasa',
+        'email'    => 'hanasa@hanasa.com',
+        'password' => bcrypt('password')
+    ]);
+
+    $companies = Company::factory(3)
+        ->state([
+            'owned_by' => $user->id
+        ])
+        ->create();
+
+    $jobs = Job::factory(8)
+        ->state([
+            'company_id' => $companies->pluck('id')->random()
+        ])
+        ->create();
+
+    $this->post(route('user.login'), [
+        'email'    => 'hanasa@hanasa.com',
+        'password' => 'password'
+    ])->json();
+});
+
+test('user should be can see personal job posts', function () {
+    $response = $this->get(route('personal-job.index'))
+        ->json();
+    $this->assertCount(8, $response['data']);
+});
+
+test('user should be can see applicants on single personal job post', function () {
+    $guest = User::factory()->create();
+    $job = Job::limit(1)->first();
+    $applyJob = ApplyJob::create([
+        'user_id'      => $guest->id,
+        'uuid'         => fake()->uuid(),
+        'job_id'       => $job->id,
+        'attachment'   => fake()->sentence(),
+        'cover_letter' => fake()->sentence(),
+        'status'       => 'pending',
+    ]);
+
+    $response = $this->get(route('personal-job.show', [
+        'job' => $job->uuid
+    ]))
+        ->json();
+
+    $this->assertCount(1, $response['data']['applicants']);
+});
